c72ef0c44125293b578dcf99e0685be28719b286
Yukuiii <472619563@qq.com>
2025-11-22 17:39:10 +0800

    Remove obsolete OSS request handling function from v3 API.

--- functions/v3/[[path]].js
+++ functions/v3/[[path]].js
@@ -3,10 +3,11 @@ const DEFAULT_OSS_HOST = 'https://qos-pic-temp.oss-cn-beijing.aliyuncs.com';
 export async function onRequest({ request, env, params }) {
   const upstreamHost = (env?.OSS_ACCESS_HOST || DEFAULT_OSS_HOST).replace(/\/$/, '');
   const url = new URL(request.url);
-  const relative = params?.path ? `/${params.path}` : url.pathname.replace(/^\/v3/, '') || '/';
+  const relative = params?.path ? (Array.isArray(params.path) ? params.path.join('/') : params.path) : url.pathname.replace(/^\/v3/, '') || '';
   const normalizedPath = relative.startsWith('/') ? relative : `/${relative}`;
   const targetUrl = `${upstreamHost}${normalizedPath}${url.search}`;
 
   const forwardRequest = new Request(targetUrl, request);
   return fetch(forwardRequest);
 }
+
